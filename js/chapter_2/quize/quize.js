677ee3a6dfbab1a76eafcfe8423fedf5c106f4bd
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-01-21 13:33:09 +0330

    add quize

--- js/chapter_2/quize/quize.js
+++ js/chapter_2/quize/quize.js
@@ -1,6 +1,22 @@
 const form = document.querySelector("form");
-const quizeOne = document.querySelector(".quize1").value;
+const quizeOne = document.querySelector(".quize1");
+const quizeTwo = document.querySelector(".quize2");
+const score = document.querySelector("span");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(form.array.value);
+
+  quizeOne.addEventListener("click", (e) => {
+    if (e.path[2]) {
+      console.log("eroro");
+    } else {
+      console.log("nooo");
+    }
+  });
+  quizeTwo.addEventListener("click", (e) => {
+    if (e.path[1]) {
+      console.log("errror");
+    } else {
+      console.log("heh");
+    }
+  });
 });
