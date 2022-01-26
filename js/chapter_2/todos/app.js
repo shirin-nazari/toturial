ed7a28cc0b87d653b4f7f4703384ddaded551bda
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-01-26 21:09:04 +0330

    to do list

---
+++ js/chapter_2/todos/app.js
@@ -0,0 +1,49 @@
+const ulList = document.querySelector("ul");
+
+const icons = document.querySelectorAll("i");
+const li = document.querySelectorAll("li");
+
+/* --------------------------- remove karadan todo -------------------------- */
+icons.forEach((i) =>
+  i.addEventListener("click", (e) => {
+    li.forEach((item) =>
+      item.addEventListener("click", (e) => {
+        item.remove();
+      })
+    );
+  })
+);
+/* -------------------------------------------------------------------------- */
+/* ------------------------ add karadan data ba input ----------------------- */
+const inputTextAdd = document.querySelector("input[type=text]");
+const buttonAdd = document.querySelector("#button-addon1");
+inputTextAdd.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  const newLi = document.createElement("li");
+  const newIcons = document.createElement("i");
+  newIcons.className = "fas fa-trash-alt float-end";
+  newLi.classList = "list-group-item";
+  newLi.textContent = e.target.value;
+  newLi.appendChild(newIcons);
+  ulList.appendChild(newLi);
+  e.target.value = "";
+  //  remove for new icons
+  newIcons.addEventListener("click", () => newLi.remove());
+});
+/* -------------------------------------------------------------------------- */
+/* ------------------------------ search button ----------------------------- */
+const searchInput = document.querySelector("input");
+const buttonSearch = document.querySelector("button");
+console.log(buttonSearch);
+const formSearch = document.querySelector("form");
+searchInput.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  li.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.target.value === item.value
+        ? (item.style.display = "block")
+        : (item.style.display = "none");
+    });
+  });
+  //   e.target.value===li.target.value
+});
