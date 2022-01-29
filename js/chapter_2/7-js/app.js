74f7b08b78297f74af4411af2dad066085afe0a8
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-01-29 13:02:19 +0330

    add file

---
+++ js/chapter_2/7-js/app.js
@@ -0,0 +1,9 @@
+const request = new XMLHttpRequest();
+request.addEventListener("readystatechange", () => {
+  //   console.log(request, request.readyState)
+  if (request.readyState === 4) {
+    console.log(request.responseText); //get jison
+  }
+});
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+request.send();
