f0daab1617566b9d3110c1ffb0a1d53418f15e69
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-01-30 20:54:27 +0330

    add callback

--- js/chapter_2/7-js/app.js
+++ js/chapter_2/7-js/app.js
@@ -1,9 +1,18 @@
-const request = new XMLHttpRequest();
-request.addEventListener("readystatechange", () => {
-  //   console.log(request, request.readyState)
-  if (request.readyState === 4) {
-    console.log(request.responseText); //get jison
-  }
-});
+const getTodo = (callback) => {
+  const request = new XMLHttpRequest();
+  request.addEventListener("readystatechange", () => {
+    //   console.log(request, request.readyState)
+    if (request.readyState === 4 && request.status === 200) {
+      // console.log(request.responseText); //get jison
+      callback();
+    } else if (request.readyState === 4) {
+      callback();
+    }
+  });
+};
 request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
 request.send();
+
+getTodo(() => {
+  console.log("callback todo");
+});
