055d111a2b99fdcf400bcdf8baa523a9c2acb0e2
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-01-30 21:19:05 +0330

    add file json

--- js/chapter_2/7-js/app.js
+++ js/chapter_2/7-js/app.js
@@ -4,15 +4,23 @@ const getTodo = (callback) => {
     //   console.log(request, request.readyState)
     if (request.readyState === 4 && request.status === 200) {
       // console.log(request.responseText); //get jison
-      callback();
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
     } else if (request.readyState === 4) {
-      callback();
+      callback("colud not fetch", undefined);
     }
   });
+  // request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  request.open("GET", "todos.json");
+  request.send();
 };
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-request.send();
 
-getTodo(() => {
-  console.log("callback todo");
+getTodo((err, data) => {
+  console.log("callback fired");
+  console.log(err, data);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
