43fc4dc7d87e3f66d9be131c709f7c3f1d5bfa8d
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-01-31 10:56:33 +0330

    finish promise

--- js/chapter_2/7-js/app.js
+++ js/chapter_2/7-js/app.js
@@ -1,29 +1,29 @@
-const getTodo = (resource, callback) => {
-  const request = new XMLHttpRequest();
-  request.addEventListener("readystatechange", () => {
-    //   console.log(request, request.readyState)
-    if (request.readyState === 4 && request.status === 200) {
-      // console.log(request.responseText); //get jison
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback("colud not fetch", undefined);
-    }
-  });
-  // request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-  request.open("GET", resource);
-  request.send();
-};
+// const getTodo = (resource, callback) => {
+//   const request = new XMLHttpRequest();
+//   request.addEventListener("readystatechange", () => {
+//     //   console.log(request, request.readyState)
+//     if (request.readyState === 4 && request.status === 200) {
+//       // console.log(request.responseText); //get jison
+//       const data = JSON.parse(request.responseText);
+//       callback(undefined, data);
+//     } else if (request.readyState === 4) {
+//       callback("colud not fetch", undefined);
+//     }
+//   });
+//   // request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+//   request.open("GET", resource);
+//   request.send();
+// };
 
-getTodo("todo/todos.json", (err, data) => {
-  console.log(data);
-  getTodo("todo/shirin.json", (err, data) => {
-    console.log(data);
-    getTodo("todo/bahar.json", (err, data) => {
-      console.log(data);
-    });
-  });
-});
+// getTodo("todo/todos.json", (err, data) => {
+//   console.log(data);
+//   getTodo("todo/shirin.json", (err, data) => {
+//     console.log(data);
+//     getTodo("todo/bahar.json", (err, data) => {
+//       console.log(data);
+//     });
+//   });
+// });
 
 /* --------------------------- call back function --------------------------- */
 // setTimeout(() => {
@@ -68,7 +68,58 @@ getTodo("todo/todos.json", (err, data) => {
 // sintax:
 
 // make function(){
-//   const flan=new Promise ()
+//   return new Promise ((resolve,reject)=>{
+// some thing
+// })
 // }
 
-// make(ye parametr ghabol mikone).then(function(){}).then(function(){}).cash(error)
+// make(ye parametr ghabol mikone).then(function(){}).then(function(){}).catch(error)
+// const getSomeThing = () => {
+//   return new Promise((resolve, reject) => {
+//     resolve("eshtebah mizani");
+//     reject("heh");
+//   });
+// };
+// getSomeThing()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// function bala ba estefade az promise
+const getTodo = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", () => {
+      //   console.log(request, request.readyState)
+      if (request.readyState === 4 && request.status === 200) {
+        // console.log(request.responseText); //get jison
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject("colud not fetch");
+      }
+    });
+    // request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+    request.open("GET", resource);
+    request.send();
+  });
+};
+getTodo("todo/shirin.json")
+  .then((data) => {
+    console.log(data, " i get shirin");
+    return getTodo("todo/bahar.json");
+  })
+  .then((data) => {
+    console.log(data, "i get bahar");
+    return getTodo("todo/todos.json");
+  })
+  .then((data) => {
+    console.log(data, "i get todos");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+/* -------------------------------- fetch API ------------------------------- */
